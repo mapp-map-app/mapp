fe70ee0e9a063827ee22d3c17b0893b9281e5a31
Andreas Christianson <achristianson@pessimistic-it.com>
2020-04-27 23:58:51 -0500

    ran the linter

--- sub-systems/websocket-server/src/controllers/rooms/:roomId/room-objects/put.ts
+++ sub-systems/websocket-server/src/controllers/rooms/:roomId/room-objects/put.ts
@@ -7,21 +7,21 @@ import { RoomObject } from '../../../../persistance/db'
 export const put = (router: Router) =>
   router.put('/rooms/:roomId/room-objects', async (ctx) => {
     const roomObect = <RoomObject>ctx.request.body
-    
+
     if (!roomObect) {
       ctx.status = status(400)
       return
     }
 
     const room = await getById(ctx.params.roomId)
-    
+
     if (!room) {
       ctx.status = status(409)
       return
     }
 
     roomObect.roomId = ctx.params.roomId
-    
+
     const updatedRoomObject = await update(roomObect)
     ctx.body = updatedRoomObject
   })
