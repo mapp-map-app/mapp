7cc7fdf9f71bcd81d2ffaf7f32a423898b715ff8
Andreas Christianson <achristianson@pessimistic-it.com>
2020-04-28 01:39:39 -0500

    switched to 4 char ids

--- sub-systems/websocket-server/src/persistance/repositories/room-objects.ts
+++ sub-systems/websocket-server/src/persistance/repositories/room-objects.ts
@@ -9,6 +9,7 @@ export const update = async (roomObject: RoomObject) => {
   const upsertedRoomObject = await roomObjects().upsert(roomObject).write()
   Server.getInstance().in(roomObject.roomId).emit('roomObjectChanged', upsertedRoomObject)
   Logger.log(`Room object ${roomObject.id} (in room ${roomObject.roomId}) changed.`, upsertedRoomObject)
+  return upsertedRoomObject
 }
 
 export const getByRoomId = (roomId: string) => roomObjects().filter({ roomId }).value()
